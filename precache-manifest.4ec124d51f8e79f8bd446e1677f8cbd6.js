2fbb2c1847bace4484f5dea460bbf6172b81e12e
Luis Suarez <suarezluis@gmail.com>
2018-10-21 16:03:51 -0500

    Updates

---
+++ precache-manifest.4ec124d51f8e79f8bd446e1677f8cbd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17a934d6f21d6a85cef4",
+    "url": "/vinDecode/static/js/runtime~main.17a934d6.js"
+  },
+  {
+    "revision": "4992d259f82db95110ac",
+    "url": "/vinDecode/static/js/main.4992d259.chunk.js"
+  },
+  {
+    "revision": "2d0e2afb9f84e8360784",
+    "url": "/vinDecode/static/js/1.2d0e2afb.chunk.js"
+  },
+  {
+    "revision": "4992d259f82db95110ac",
+    "url": "/vinDecode/static/css/main.5603253c.chunk.css"
+  },
+  {
+    "revision": "1affb5faee15bab62c15a6e9f32f396c",
+    "url": "/vinDecode/index.html"
+  }
+];

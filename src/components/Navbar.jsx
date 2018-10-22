b18b493d3d53f9aec3f00a007fc002858847c5b2
Luis Suarez <suarezluis@gmail.com>
2018-10-22 14:46:29 -0500

    removed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <img src="./img/roadvantage.png" height="40" alt="" />
+        {/* <img src="./img/roadvantage.png" height="40" alt="" /> */}
         VIN Tool
       </nav>
     );
